6a82884dce2074241b25756b8b32ce69b87e9c70
song_xu <s2jackson@hotmail.com>
2014-12-11 21:28:31 -0500

    updata script

--- run.sh
+++ run.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
-# ./crawl.sh data/brands.json proxy.txt
 # 具体参数设置见jd_config.py
+# 如果快速测试，请跳过1（利用已经爬取的数据，节省数据爬取的时间消耗）
+
 #1. 京东数据抓取
 python jd_wap.py
-
 
 #2. 数据预处理
 python jd_process.py
 
 # 3.分类
 python jd_classify.py
+
+
+
+
+
 
 
